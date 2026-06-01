b62218cb3539881bb91106551ea9f1bc7c7ad0b1
Fraeltas <sagitarius.fzm@gmail.com>
2026-06-01 09:04:54 -0500

    fix import mcstatus.server

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 import discord
 from discord import app_commands
 from discord.ext import commands, tasks
-from mcstatus import MinecraftServer   # librería para ping al server
+from mcstatus.server import MinecraftServer  # librería para ping al server
 
 intents = discord.Intents.default()
 intents.message_content = True
